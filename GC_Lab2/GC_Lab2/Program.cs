c33a0c871873c826a8bce87237ff89f4dcd3c3ef
Christy Hartner <xtyvkv@gmail.com>
2022-04-20 02:54:16 -0400

    WHY ARE THERE SO MANY FILES OH MY GOD...progress on the logic tho

---
+++ GC_Lab2/GC_Lab2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class GC_Lab2
+{
+    static void Main(string[] args)
+    {
+        int inputInt;
+        Console.WriteLine("Please enter an integer.");
+        string input = Console.ReadLine();
+        if (!int.TryParse(input, out inputInt))
+        {
+            Console.WriteLine("Not an integer.");
+            Console.WriteLine("Bye!!!!!!!!!");
+
+        }
+        else
+        {
+        powers powerTable = new powers();
+        Console.WriteLine("Number\t Squared\t Cubed");
+        Console.WriteLine("=======\t =======\t =======");
+        Console.WriteLine($"{inputInt}\t {powerTable.Squared(inputInt)}\t\t {powerTable.Cube(inputInt)}");
+        }
+    }
+}
+public class powers
+{
+    // Square function
+    public int Squared(int inputInt)
+    {
+        return inputInt * inputInt;
+    }
+    // Cube function
+    public int Cube(int inputInt)
+    {
+        return inputInt * inputInt * inputInt;
+    }
+}
+
