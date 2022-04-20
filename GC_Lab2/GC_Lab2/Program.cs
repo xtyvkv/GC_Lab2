a20c70615bdcf2c170d68ef3292be678cadfaaec
Christy Hartner <xtyvkv@gmail.com>
2022-04-20 16:01:59 -0400

    additional logic

--- GC_Lab2/GC_Lab2/Program.cs
+++ GC_Lab2/GC_Lab2/Program.cs
@@ -18,17 +18,30 @@ public class GC_Lab2
             {
                 Console.WriteLine("Invalid input.");
             }
+            /*
+            else if (inputInt >= Int32.MaxValue)
+            {
+                Console.WriteLine("You have exceeded the minimum or maximum number whose cube will fit in an integer.");
+            }
+            /* why does this only work if i enter the exact number. it should work for any number greater */
+            else
+
+                if (inputInt >= Int32.MaxValue)
+                {
+                    Console.WriteLine("You have exceeded the minimum or maximum number whose cube will fit in an integer.");
+                }
             else
             {
                 powers powerTable = new powers();
                 Console.WriteLine();
                 Console.WriteLine($"Nice! {inputInt}! Look, it may be backwards, but here is a nice table for you:");
                 Console.WriteLine();
-                Console.WriteLine("Number\t Squared\t Cubed");
-                Console.WriteLine("=======\t =======\t =======");
+                Console.WriteLine("Number  Squared   Cubed");
+                Console.WriteLine("======  =======  ======");
+
                 while (inputInt > 0)
                 {
-                    Console.WriteLine("{0}\t {1}\t\t {2}", inputInt, powerTable.Squared(inputInt), powerTable.Cube(inputInt));
+                    Console.WriteLine(String.Format("{0,6} {1,8} {2,7}", inputInt, powerTable.Squared(inputInt), powerTable.Cube(inputInt)));
                     inputInt--;
                 }
             }
@@ -61,5 +74,4 @@ public class powers
         return inputInt * inputInt * inputInt;
     }
 }
-
 
