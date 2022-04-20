edd0a851dda91e93491babff071a5d6214d7f57a
Christy Hartner <xtyvkv@gmail.com>
2022-04-20 02:59:56 -0400

    aggressive superficial update

--- GC_Lab2/GC_Lab2/Program.cs
+++ GC_Lab2/GC_Lab2/Program.cs
@@ -9,16 +9,19 @@ public class GC_Lab2
         string input = Console.ReadLine();
         if (!int.TryParse(input, out inputInt))
         {
-            Console.WriteLine("Not an integer.");
+            Console.WriteLine("Not an integer. And I DON'T WANT YOU TO TRY AGAIN BECAUSE I'M TIRED AND I WORK TOO MUCH!!!");
             Console.WriteLine("Bye!!!!!!!!!");
 
         }
         else
         {
-        powers powerTable = new powers();
-        Console.WriteLine("Number\t Squared\t Cubed");
-        Console.WriteLine("=======\t =======\t =======");
-        Console.WriteLine($"{inputInt}\t {powerTable.Squared(inputInt)}\t\t {powerTable.Cube(inputInt)}");
+            powers powerTable = new powers();
+            Console.WriteLine();
+            Console.WriteLine($"Congratulations! {inputInt}!!!! You get to live. If you had entered a letter or blank/white space\n(I know the code to deal with that but I'm too tired and braindead to implement it)\nI would've punched you in your eyes.");
+            Console.WriteLine();
+            Console.WriteLine("Number\t Squared\t Cubed");
+            Console.WriteLine("=======\t =======\t =======");
+            Console.WriteLine($"{inputInt}\t {powerTable.Squared(inputInt)}\t\t {powerTable.Cube(inputInt)}");
         }
     }
 }
