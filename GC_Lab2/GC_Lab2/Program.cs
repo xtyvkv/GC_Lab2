18bcf85d4c315b0a18feb63deb91aa1ee063b0fa
Christy Hartner <xtyvkv@gmail.com>
2022-04-20 04:22:47 -0400

    making it passable but this is temporary because i'll make it better tomorrow

--- GC_Lab2/GC_Lab2/Program.cs
+++ GC_Lab2/GC_Lab2/Program.cs
@@ -4,30 +4,48 @@ public class GC_Lab2
 {
     static void Main(string[] args)
     {
-        int inputInt;
-        bool inputIsWithin = true;
-        Console.WriteLine("Please enter an integer.");
-        string input = Console.ReadLine();
-        if (!int.TryParse(input, out inputInt))
-        {
-            Console.WriteLine("Not an integer. And I DON'T WANT YOU TO TRY AGAIN BECAUSE I'M TIRED AND I WORK TOO MUCH!!!");
-            Console.WriteLine("Bye!!!!!!!!!");
+        bool go = true;
 
-        }
-        else
+        Console.WriteLine("Learn your SQUARES AND CUBES >:[");
+        Console.WriteLine();
+
+        while (go == true)
         {
-            powers powerTable = new powers();
-            Console.WriteLine();
-            Console.WriteLine($"Congratulations! {inputInt}!!!! You get to live. If you had entered a letter or blank/white space\n(I know the code to deal with that but I'm too tired and braindead to implement it)\nI would've punched you in your eyes.");
-            Console.WriteLine();
-            Console.WriteLine("Number\t Squared\t Cubed");
-            Console.WriteLine("=======\t =======\t =======");
-            while (inputInt > 0)
+            int inputInt;
+            Console.WriteLine("Enter an integer:");
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out inputInt))
             {
-                Console.WriteLine("{0}\t {1}\t\t {2}", inputInt, powerTable.Squared(inputInt), powerTable.Cube(inputInt));
-                inputInt--;
+                Console.WriteLine("Invalid input.");
+            }
+            else
+            {
+                powers powerTable = new powers();
+                Console.WriteLine();
+                Console.WriteLine($"Nice! {inputInt}! Look, it may be backwards, but here is a nice table for you:");
+                Console.WriteLine();
+                Console.WriteLine("Number\t Squared\t Cubed");
+                Console.WriteLine("=======\t =======\t =======");
+                while (inputInt > 0)
+                {
+                    Console.WriteLine("{0}\t {1}\t\t {2}", inputInt, powerTable.Squared(inputInt), powerTable.Cube(inputInt));
+                    inputInt--;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("If you would like to try again, press Y.");
+            string goAgain = Console.ReadLine();
+            if (goAgain.ToUpper() != "Y")
+            {
+                go = false;
+            }
+            else
+            {
+                Console.WriteLine();
             }
         }
+        Console.WriteLine();
+        Console.WriteLine("BYE THEN.");
     }
 }
 public class powers
